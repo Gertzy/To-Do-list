376a9fb3326320e67cbccb24de2e2cfb9bd9f743
Noah Goertzen <noah.goertzen@gmail.com>
2025-07-05 18:08:34 -0500

    Adding functions

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log("Hello world!");
+const button = document.querySelector("#button");
+const centered = document.querySelector("#centered");
+
+button.addEventListener("click", () => {
+  const type = document.createElement("h3");
+  const text = document.createTextNode("Task");
+  type.appendChild(text);
+  centered.appendChild(type);
+});
