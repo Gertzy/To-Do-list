fff96b098ed2584cec21291c03ce42b89370e899
Noah Goertzen <noah.goertzen@gmail.com>
2025-07-06 13:28:42 -0500

    Improved adding function

--- script.js
+++ script.js
@@ -2,8 +2,14 @@ const button = document.querySelector("#button");
 const centered = document.querySelector("#centered");
 
 button.addEventListener("click", () => {
-  const type = document.createElement("h3");
-  const text = document.createTextNode("Task");
-  type.appendChild(text);
-  centered.appendChild(type);
+  let box = document.getElementById("input").value;
+  if (box.trim() !== "") {
+    const type = document.createElement("h3");
+    const text = document.createTextNode(box);
+    type.appendChild(text);
+    centered.appendChild(type);
+    document.getElementById("input").value = "";
+  } else {
+    console.log("invalid");
+  }
 });
