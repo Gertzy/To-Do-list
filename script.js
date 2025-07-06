64c0a55f1f9e745dc28a2843bc89a92b094b3da5
Noah Goertzen <slimyleaf@proton.me>
2025-07-06 18:25:53 -0500

    Checkbox addition

--- script.js
+++ script.js
@@ -4,11 +4,20 @@ const centered = document.querySelector("#centered");
 button.addEventListener("click", () => {
   let box = document.getElementById("input").value;
   if (box.trim() !== "") {
+    // Making text for task
+    const div = document.createElement("div");
     const type = document.createElement("h3");
     const text = document.createTextNode(box);
+    div.setAttribute("id", "taskbox");
     type.appendChild(text);
-    centered.appendChild(type);
+    div.appendChild(type);
+    centered.appendChild(div);
     document.getElementById("input").value = "";
+    // Making buttons
+    const checkButton = document.createElement("input");
+    checkButton.setAttribute("type", "checkbox");
+    checkButton.setAttribute("id", "checkbox");
+    div.appendChild(checkButton);
   } else {
     console.log("invalid");
   }
