5fe34fe09314bdbd74cd6133b1c39de528ad0502
Noah Goertzen <noah.goertzen@gmail.com>
2025-07-09 21:39:36 -0500

    more button sty

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ button.addEventListener("click", () => {
     const div = document.createElement("div");
     const type = document.createElement("h3");
     const text = document.createTextNode(box);
-    div.setAttribute("id", "taskbox");
+    div.setAttribute("class", "taskbox");
     type.appendChild(text);
     div.appendChild(type);
     centered.appendChild(div);
@@ -16,16 +16,19 @@ button.addEventListener("click", () => {
     // Making check button
     const checkButton = document.createElement("input");
     checkButton.setAttribute("type", "checkbox");
-    checkButton.setAttribute("id", "checkbox");
+    checkButton.setAttribute("class", "checkbox");
     div.appendChild(checkButton);
     // Delete button
     const deleteButton = document.createElement("button");
+    const buttonWrapper = document.createElement("div");
     deleteButton.textContent = "Delete";
     deleteButton.setAttribute("class", "deletebutton");
+    buttonWrapper.setAttribute("class", "buttonwrapper");
+    buttonWrapper.appendChild(deleteButton);
+    div.appendChild(buttonWrapper);
     deleteButton.addEventListener("click", () => {
       centered.removeChild(div);
     });
-    div.appendChild(deleteButton);
   } else {
     console.log("invalid");
   }
